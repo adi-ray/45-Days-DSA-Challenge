5093504856ea42bafa6399b3c86df8c064be1013
adi-ray <adityadns03@gmail.com>
2024-07-15 18:48:03 +0530

    283-Array

---
+++ Day-01/Move-Zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int j = -1;
+
+        for(int i=0;i<n;i++) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // all non-zero elements
+        if (j == -1) 
+            return;
+
+        for (int i = j+1; i<n; i++) {
+            if (nums[i] != 0) {
+                // swapping
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;          
+                j++;
+            }
+        }
+    }
+}
