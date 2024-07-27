afe53f2624a1a538569e74a30adda20be86aad36
adi-ray <adityadns03@gmail.com>
2024-07-27 21:26:22 +0530

    22-String

---
+++ Day-13/Generate-Parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    private void dfs(int openP, int closeP, String s, int n, List<String> res) {
+        if (openP + closeP == n * 2) {
+            res.add(s);
+            return;
+        }
+
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(", n, res);
+        }
+
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")", n, res);
+        }
+    }    
+    
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs(0, 0, "", n, res);
+
+        return res;        
+    }
+}
