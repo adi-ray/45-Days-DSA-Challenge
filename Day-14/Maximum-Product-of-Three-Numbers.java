eb0a29e5ce183e22b7464dc406c1a88c5c1a70fd
adi-ray <adityadns03@gmail.com>
2024-07-28 12:11:14 +0530

    628-Mathematical

---
+++ Day-14/Maximum-Product-of-Three-Numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int m1 = nums[n-1]*nums[n-2]*nums[n-3];
+        int m2 = nums[0]*nums[1]*nums[n-1];
+        
+        return m1>m2 ? m1 : m2;
+    }
+}
